c9e4387daa58ebb27f40ce634e6cfdc65a8c8f78
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-11-16 17:37:10 +0530

    lets write code

---
+++ Season1/index.js
@@ -0,0 +1,12 @@
+let name="Manoj";
+let a=10;
+let b=20;
+console.log(`name is ${name}`);
+console.log(`value of a is ${a}`);
+console.log(`value of b is ${b}`);
+
+console.log(global);
+
+console.log(this);
+
+console.log(globalThis)
