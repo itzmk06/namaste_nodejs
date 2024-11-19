9dd391b44573c0b17cf6c89756f13796ef1854db
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-11-19 20:34:20 +0530

    async io libuv 06

--- Season1/index.js
+++ Season1/index.js
@@ -1,23 +1,27 @@
 // const {calculateSum,message,calculateProduct}=require("./app.js");
-const {calculateProduct,calculateSum}=require("./calculate");
-const data=require("./data.json");
+// const {calculateProduct,calculateSum}=require("./calculate");
+// const data=require("./data.json");
 
-let name="Manoj";
-let a=10;
-let b=20;
-console.log(`name is ${name}`);
-console.log(`value of a is ${a}`);
-console.log(`value of b is ${b}`);
+// let name="Manoj";
+// let a=10;
+// let b=20;
+// console.log(`name is ${name}`);
+// console.log(`value of a is ${a}`);
+// console.log(`value of b is ${b}`);
 
-console.log(global);
+// console.log(global);
 
-console.log(this);
+// console.log(this);
 
-console.log(globalThis)
+// console.log(globalThis)
 
-calculateSum(10,20);
-calculateProduct(10,20);
-console.log(data);
+// calculateSum(10,20);
+// calculateProduct(10,20);
+// console.log(data);
+(
+function(){
+    console.log("This is the IIFE function which is called immediately!");
+})();
 
 
 // console.log(message);
